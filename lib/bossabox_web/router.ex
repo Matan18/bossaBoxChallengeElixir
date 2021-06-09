4158195d11b1d686c4abda045c122f54926c0042
Mateus Andriola <mateusandriola18@gmail.com>
2021-06-09 19:23:32 -0300

    Remove html views

--- lib/bossabox_web/router.ex
+++ lib/bossabox_web/router.ex
@@ -1,22 +1,8 @@
 defmodule BossaboxWeb.Router do
   use BossaboxWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
-  end
-
-  scope "/", BossaboxWeb do
-    pipe_through :browser
-
-    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
@@ -37,7 +23,7 @@ defmodule BossaboxWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through :browser
+      pipe_through [:fetch_session, :protect_from_forgery]
       live_dashboard "/dashboard", metrics: BossaboxWeb.Telemetry
     end
   end
